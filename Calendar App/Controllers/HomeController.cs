32fcdc51abb57a19c79ff16e13da6e6d3a11f8a4
BharatKumarPamarthi <BharatKumarPamarthi@IKCON1052>
2023-03-15 19:28:47 +0530

    Add project files.

---
+++ Calendar App/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Calendar_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        string credentialsFile = "C:\\Users\\BharatKumarPamarthi\\Documents\\Bharat Desktop\\Calendar App\\Calendar App\\credentials.json";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OauthRedirect()
+        {
+            JObject credentials = JObject.Parse(System.IO.File.ReadAllText(credentialsFile));
+            var redirectUrl = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize?" +
+                             "&scope=" + credentials["scopes"].ToString() +
+                             "&response_type =code" +
+                             "&response_mode = query" +
+                             "&state=12345" +
+                             "&redirect_uri=" + credentials["redirect_url"].ToString() +
+                             "&client_id=" + credentials["client_id"].ToString();
+
+
+
+
+            return Redirect(redirectUrl);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Calendar App/Controllers/OauthController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace Calendar_App.Controllers
+{
+    public class OauthController : Controller
+    {
+        string credentialsFile = "C:\\Users\\BharatKumarPamarthi\\Documents\\Bharat Desktop\\Calendar App\\Calendar App\\credentials.json";
+        string tokensfile = "C:\\Users\\BharatKumarPamarthi\\Documents\\Bharat Desktop\\Calendar App\\Calendar App\\tokens.json";
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Callback(string code, string state, string error)
+        {
+            JObject credentials = JObject.Parse(System.IO.File.ReadAllText(credentialsFile));
+            if (!string.IsNullOrWhiteSpace(code))
+            {
+                //RestClient restClient = new RestClient();
+                RestRequest restRequest = new RestRequest();
+
+                restRequest.AddParameter("client_id", credentials["client_id"].ToString());
+                restRequest.AddParameter("scope", credentials["scopes"].ToString());
+                restRequest.AddParameter("redirect_uri", credentials["redirect_url"].ToString());
+                restRequest.AddParameter("code", code);
+                restRequest.AddParameter("grant_type", "authorization_code");
+                restRequest.AddParameter("client_secret",credentials["client_secret"].ToString());
+
+                var httpClient = new HttpClient{ BaseAddress = new Uri("https://login.microsoftonline.com/organizations/oauth2/v2.0/token") };
+                RestClient client = new RestClient(httpClient, new RestClientOptions(httpClient.BaseAddress));
+               
+
+                var response = client.Post(restRequest);
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    System.IO.File.WriteAllText(tokensfile, response.Content);
+                    return RedirectToAction("Index", "Home");
+                }
+
+
+
+            }
+            return RedirectToAction("Error");
+
+        }
+    }
+}
